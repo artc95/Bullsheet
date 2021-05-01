074e0486d666bde7d79f99ed5c08123ac74b3ba8
Arthur Chionh (Hwai Teck) <arthur95chionh@gmail.com>
2021-05-01 21:08:18 +0800

    Test Heroku using sample Dash app

--- bull.py
+++ bull.py
@@ -6,6 +6,11 @@ import hashlib
 import datetime, time
 import pandas as pd
 
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
+
 nonce = int(round(time.time()*1000))
 
 #sandbox api endpoint
@@ -39,6 +44,7 @@ headers = {
 r = requests.request("POST", url, headers=headers)
 
 trades = r.json()
+print(trades)
 
 timestampms = []
 buysell = []
@@ -55,7 +61,7 @@ for trade in trades:
     currency.append(trade["fee_currency"])
     price.append(trade["price"])
     qty.append(trade["amount"])
-    value.append(float(trade["fee_amount"])/0.007)
+    value.append(float(trade["fee_amount"])/0.01)
 
 trades_dict = {
     "timestamp_ms": timestampms,
@@ -70,3 +76,34 @@ trades_dict = {
 trades_df = pd.DataFrame(trades_dict, columns = ["timestamp_ms", "buysell", "symbol", "currency", "price", "qty", "value"])
 
 print(trades_df)
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
