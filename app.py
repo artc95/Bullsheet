c2a5af0961f4be7fbe5b9364ce604cbef9a83bdd
Arthur Chionh (Hwai Teck) <arthur95chionh@gmail.com>
2021-05-02 17:17:51 +0800

    Create Dash_table of trades

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-import pandas as pd
 import dash
+from dash.dependencies import Input, Output
+import dash_table
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.express as px
+
+import pandas as pd
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -13,21 +15,15 @@ server = app.server
 # see https://plotly.com/python/px-arguments/ for more options
 df = pd.read_csv("https://raw.githubusercontent.com/artc95/Bullsheet/master/trades.csv")
 
-def generate_table(dataframe):
-    return html.Table([
-        html.Thead(
-            html.Tr([html.Th(col) for col in dataframe.columns])
-        ),
-        html.Tbody([
-            html.Tr([
-                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
-            ]) for i in range(len(dataframe))
-        ])
-    ])
-
-app.layout = html.Div(children=[
-    html.H1(children='Trades'),
-    generate_table(df)
+app.layout = html.Div([
+    dash_table.DataTable(
+        id="trades_table",
+        columns=[{"name":i, "id":i} for i in df.columns],
+        data=df.to_dict("records"),
+        filter_action="native",
+        sort_action="native",
+    ),
+    html.Div(id="trades_table_container")
 ])
 
 if __name__ == '__main__':
