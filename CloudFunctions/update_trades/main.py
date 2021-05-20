3956e877a78bffe557156df5c2b0300fa0bb2c1e
Chionh Hwai Teck (ARTHUR) <arthur95chionh@gmail.com>
2021-05-20 12:15:18 +0800

    Changed to update_trades
    - Updated table in table_ref
    - Updated job_config.write_disposition from WRITE_TRUNCATE to WRITE_APPEND
    - Updated uri

--- CloudFunctions/update_trades/main.py
+++ CloudFunctions/update_trades/main.py
@@ -1,12 +1,12 @@
-def update_net(data,context):
+def update_trades(data,context):
     from google.cloud import bigquery
     client = bigquery.Client()
-    table_ref = client.dataset("Bullsheet").table("net")
+    table_ref = client.dataset("Bullsheet").table("trades")
     job_config = bigquery.LoadJobConfig()
-    job_config.write_disposition = bigquery.WriteDisposition.WRITE_TRUNCATE # this job will truncate table data and write from the beginning
+    job_config.write_disposition = bigquery.WriteDisposition.WRITE_APPEND # this job will truncate table data and write from the beginning
     job_config.skip_leading_rows = 1
     job_config.source_format = bigquery.SourceFormat.CSV
 
-    uri = "gs://bullsheet/net.csv"
+    uri = "gs://bullsheet/trades.csv"
     load_job = client.load_table_from_uri(uri, table_ref, job_config=job_config)
     load_job.result()
